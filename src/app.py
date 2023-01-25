f9f2664598183662c9559ea5b74470c92083578b
sduartecor <santiagoduartecor@outlook.com>
2023-01-25 19:07:13 +0000

    Version final

--- src/app.py
+++ src/app.py
@@ -2,30 +2,38 @@ from flask import Flask, jsonify, request
 import json
 app = Flask(__name__)
 
+# Diccionario
 todos = [
     { "label": "My first task", "done": False },
     { "label": "My second task", "done": False }
 ]
 
+# Endpoint (route) - Metodo: Get
 @app.route('/todos', methods=['GET'])
 def hello_world():
+     # Retorna la lista completa
     return jsonify(todos)
 
+# Endpoint (route) - Metodo: Post
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request.get_json(force=True)
     request_body = request.data
-
+    # Json.loads sirve para decodificar el request.data
     decoded_object = json.loads(request_body)
+    # Insertar un elemento al final de la lista
     todos.append(decoded_object)
-    
-    print("Incoming request with the following body", request_body)
-    return jsonify(todos)
+    # Jsonify: Cast de JSON
+    json_text = jsonify(todos)
+    # Retorna la lista completa
+    return json_text
 
+# Endpoint (route) - Metodo: Delete
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
+    # Elimina item de esa posición
     del todos[position]
-    print("This is the position to delete: ",position)
+     # Retorna la lista completa
     return jsonify(todos)
 
 if __name__ == '__main__':
